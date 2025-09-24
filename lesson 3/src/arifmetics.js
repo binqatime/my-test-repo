98a9d2f5706e8d6d48e3b75a52f3efb7dbfb6a09
Maria Hana <binqatime@gmail.com>
2025-09-24 20:37:08 +0300

    completed HW for lesson 3

---
+++ lesson 3/src/arifmetics.js
@@ -0,0 +1,41 @@
+const a = 8;
+const b = 3;
+const c = '3';
+const d = 'number';
+let abc;
+let sum = a + b;
+console.log(sum);
+
+console.log(a - b);
+console.log(b / a);
+console.log(b + c);
+console.log(b * c);
+console.log(8 % b);
+
+abc = c + ' ' + d + ' ' + sum;
+console.log(abc);
+
+sum += 100;
+console.log(sum);
+sum = sum ** 2;
+console.log(sum);
+
+sum = ( sum / a) * b;
+console.log(sum);
+sum++;
+console.log(sum);
+
+let number = 1;
+number--;
+console.log(number);
+
+number += 9;
+number *=10;
+console.log(number);
+
+let finalResult = d + ' ' + 'is' + ' ' + number;
+console.log(finalResult);
+
+const s = 5 + "10";
+console.log(typeof s);
+console.log(s);
---
+++ lesson 3/src/logical.js
@@ -0,0 +1,23 @@
+const a = 8;
+const b = 3;
+const c = '3';
+const d = 'number';
+let abc;
+let sum = a + b;
+console.log(sum);
+console.log(a <= sum);
+console.log(a === b);
+console.log(b >= a);
+console.log(b == c);
+console.log(b === c);
+console.log(sum !== a);
+
+console.log("----------");
+
+const bool = Boolean("false");
+console.log(bool);
+
+let dogHungry = false;
+let catThirsty = true;
+
+console.log(catThirsty == dogHungry);
