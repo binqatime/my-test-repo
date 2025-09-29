1bab9938ae20a4bb79d880da9e2eeeff543473c6
Maria Hana <binqatime@gmail.com>
2025-09-29 10:18:16 +0300

    Removed unnecessary line

--- lesson 4/src/objects.js
+++ lesson 4/src/objects.js
@@ -1,54 +1,51 @@
 let School = new Object();
 
 School = {
-    subjects: {
-        subject1: 'math',
-        subject2: 'history',
-        subject3: 'geography'
-    },
-    students: [
+  subjects: {
+    subject1: "math",
+    subject2: "history",
+    subject3: "geography",
+  },
+  students: [
+    {
+      first: [
         {
-            first: [
-                {
-                    name: 'Martha',
-                    favoriteSubject: 'geography'
-                }
-            ]
+          name: "Martha",
+          favoriteSubject: "geography",
         },
-        {
-            second: [
-                {
-                    name: 'John',
-                    favoriteSubject: 'math'
-                }
-            ]
-        }
-    ],
-
-    showSubjects() {
-        console.log(this.subjects.subject1);
-        console.log(this.subjects.subject2);
-        console.log(this.subjects.subject3);
+      ],
     },
+    {
+      second: [
+        {
+          name: "John",
+          favoriteSubject: "math",
+        },
+      ],
+    },
+  ],
 
-    getStudentsFavSubjects() {
-        console.log(
-            'Favorite subject of ' +
-            this.students[0].first[0].name +
-            ' is ' +
-            this.students[0].first[0].favoriteSubject
-        );
-        console.log(
-            'Favorite subject of ' +
-            this.students[1].second[0].name +
-            ' is ' +
-            this.students[1].second[0].favoriteSubject
-        );
-    }
+  showSubjects() {
+    console.log(this.subjects.subject1);
+    console.log(this.subjects.subject2);
+    console.log(this.subjects.subject3);
+  },
+
+  getStudentsFavSubjects() {
+    console.log(
+      "Favorite subject of " +
+        this.students[0].first[0].name +
+        " is " +
+        this.students[0].first[0].favoriteSubject,
+    );
+    console.log(
+      "Favorite subject of " +
+        this.students[1].second[0].name +
+        " is " +
+        this.students[1].second[0].favoriteSubject,
+    );
+  },
 };
 
-console.log(School.students[0].first[0].name);
 School.showSubjects();
 School.getStudentsFavSubjects();
-
-
