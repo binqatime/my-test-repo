d9f5d7a2f6cb3d3647d8e67cbe9ad12d6e4f4736
Maria Hana <binqatime@gmail.com>
2025-10-02 11:00:54 +0300

    finished home work for lesson 5

---
+++ lesson 5/src/functions.js
@@ -0,0 +1,45 @@
+const numbersArr = [12, 4, 67, 90, 100, 135];
+
+function sumArrElements(arr) {
+    let sum = 0;
+    for (const num of arr) {
+        sum += num;
+    }
+
+    return sum;
+}
+
+console.log(sumArrElements(numbersArr));
+
+function getSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(getSum(numbersArr));
+
+function arrSumUp(arr) {
+    let sum = 0;
+    arr.forEach((num) => (sum += num));
+    return sum;
+}
+
+console.log(arrSumUp(numbersArr));
+
+const sports = ['football', 'tennis', 'speedskating', 'weight-lifting'];
+const popularityRate = [8, 9, 5, 7, 1];
+
+function twoArrIntoOne(arr1, arr2) {
+    for (const index in (arr1, arr2)) {
+        const a = arr1[index];
+        const b = arr2[index];
+
+        if (arr1[index] === undefined || arr2[index] === undefined) break;
+        console.log(a + ' ' + b);
+    }
+}
+
+twoArrIntoOne(sports, popularityRate);
---
+++ lesson 5/src/getter-setter.js
@@ -0,0 +1,56 @@
+let School = new Object();
+
+School = {
+    schoolName: 'High Secondary School',
+    schoolRating: 8,
+
+    get() {
+        return this.schoolRating;
+    },
+
+    set(rating) {
+        this.schoolRating = rating;
+    },
+
+    subjects: {
+        subject1: 'math',
+        subject2: 'history',
+        subject3: 'geography'
+    },
+    students: [
+        {
+            first: [
+                {
+                    name: 'Martha',
+                    favoriteSubject: 'geography'
+                }
+            ]
+        },
+        {
+            second: [
+                {
+                    name: 'John',
+                    favoriteSubject: 'math'
+                }
+            ]
+        }
+    ],
+
+    showSubjects() {
+        console.log(this.subjects.subject1);
+        console.log(this.subjects.subject2);
+        console.log(this.subjects.subject3);
+    },
+
+    getStudentsFavSubjects() {
+        console.log('Favorite subject of ' + this.students[0].first[0].name + ' is ' + this.students[0].first[0].favoriteSubject);
+        console.log('Favorite subject of ' + this.students[1].second[0].name + ' is ' + this.students[1].second[0].favoriteSubject);
+    }
+};
+
+School.showSubjects();
+School.getStudentsFavSubjects();
+
+console.log(School.schoolRating);
+School.schoolRating = 10;
+console.log(School.schoolRating);
