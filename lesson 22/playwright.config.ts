2bc43ca6d4d67e9703734e5220a86ac42c76318d
Maria Hana <binqatime@gmail.com>
2025-12-17 14:30:43 +0200

    added screen shot on fail

--- lesson 22/playwright.config.ts
+++ lesson 22/playwright.config.ts
@@ -38,7 +38,10 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
-        headless: false
+        headless: false,
+        video: 'retain-on-failure',
+        screenshot: 'only-on-failure'
+
     },
 
     /* Configure projects for major browsers */
